88ade3bc68ce70f56b9c01879b4e2f26b1e25ea5
RichardHpa <richard.hpa@hotmail.com>
2019-09-17 17:56:50 +1200

    prepared all of grunt

---
+++ Gruntfile.js
@@ -0,0 +1,128 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        sass:{
+            front: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none',
+                    noCache: true
+                },
+                files : {
+                    'assets/css/style.css':'assets/scss/front/style.scss'
+                }
+            },
+            admin: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none',
+                    noCache: true
+                },
+                files : {
+                    'assets/css/admin.css':'assets/scss/admin/admin.scss'
+                }
+            },
+            bootstrap: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none',
+                    noCache: true
+                },
+                files : {
+                    'assets/css/bootstrap.css':'assets/scss/bootstrap.scss'
+                }
+            }
+        },
+        csslint: {
+            lax: {
+                options: {
+                    import: 2
+                },
+                src: [
+                    'assets/css/*.css',
+                    '!assets/css/*.min.css',
+                    '!assets/css/bootstrap.css'
+                ]
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'assets/css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        jshint: {
+            files: ['assets/js/*/*.js'],
+            options: {
+                globals:{
+                    jQuery: true
+                }
+            }
+        },
+        uglify: {
+            front: {
+                options: {
+                    mangle: false
+                },
+                files: [{
+                    expand: true,
+                    src: ['assets/js/front/*.js', '!assets/js/front/*.min.js'],
+                    dest: 'assets/js/front',
+                    cwd: '.',
+                    rename: function (dst, src) {
+                      return src.replace('.js', '.min.js');
+                    }
+                }]
+            },
+            admin: {
+                options: {
+                    mangle: false
+                },
+                files: [{
+                    expand: true,
+                    src: ['assets/js/admin/*.js', '!assets/js/admin/*.min.js'],
+                    dest: 'assets/js/admin',
+                    cwd: '.',
+                    rename: function (dst, src) {
+                      return src.replace('.js', '.min.js');
+                    }
+                }]
+            }
+        },
+        watch : {
+            frontStyle: {
+                files : [ 'assets/scss/front/*.scss' ],
+                tasks : [ 'sass:front', 'csslint', 'cssmin' ]
+            },
+            adminStyle: {
+                files : [ 'assets/scss/admin/admin.scss' ],
+                tasks : [ 'sass:admin', 'csslint', 'cssmin' ]
+            },
+            bootstrap: {
+                files : [ 'assets/sass/bootstrap.scss' ],
+                tasks : [ 'sass:bootstrap', 'csslint', 'cssmin' ]
+            },
+            frontJS : {
+               files : ['assets/js/front/script.js'],
+               tasks : ['jshint', 'uglify:front']
+           },
+           adminJS : {
+              files : ['assets/js/admin/admin.js'],
+              tasks : ['jshint', 'uglify:admin']
+           }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+};
